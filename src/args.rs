08f09c14c208909a3f3579529086431ba658d5ad
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-11-08 13:12:22 +0300

    for list command argument --upgradable cannot be used with --orpaned

--- src/args.rs
+++ src/args.rs
@@ -6,11 +6,11 @@ pub enum Args {
     List {
         /// Print list of packages that were updated in the repo
         /// (This does not affect the local index)
-        #[clap(long, short)]
+        #[clap(long, short, conflicts_with = "orphaned")]
         upgradable: bool,
         /// Print list of orphaned packages
         /// (packages which not installed explicitly and on which no package depends)
-        #[clap(long, short)]
+        #[clap(long, short, conflicts_with = "upgradable")]
         orphaned: bool,
     },
     /// Install packages

187ee9bc01980ad9bbf9c9cef51dbbe62618d45b
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-11-10 17:22:25 +0300

    take root password from user for some commands

--- src/pacman.rs
+++ src/pacman.rs
@@ -3,13 +3,6 @@ use crate::Cmd;
 pub const PACMAN_BIN: &str = "pacman";
 const PARU_BIN: &str = "paru";
 
-pub fn program_is_present() -> anyhow::Result<bool> {
-    Ok(Cmd::new("which")
-        .arg(PACMAN_BIN)
-        .hide_error_from_user_and_give_exit_status()?
-        .success())
-}
-
 pub fn pacman() -> Cmd {
     Cmd::new(PACMAN_BIN)
 }
@@ -17,6 +10,13 @@ pub fn pacman() -> Cmd {
 pub fn paru_or_pacman() -> anyhow::Result<Cmd> {
     let cmd = program_is_present()?
         .then(|| Cmd::new(PARU_BIN))
-        .unwrap_or_else(pacman);
+        .unwrap_or_else(|| Cmd::new("sudo").arg(PACMAN_BIN));
     Ok(cmd)
+}
+
+fn program_is_present() -> anyhow::Result<bool> {
+    Ok(Cmd::new("which")
+        .arg(PACMAN_BIN)
+        .hide_error_from_user_and_give_exit_status()?
+        .success())
 }

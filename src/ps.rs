878f49902f7267cf1e81923a1053be6705a95050
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-11-18 17:10:30 +0300

    pacrs ps: show also pid and owner of process in table

--- src/ps.rs
+++ src/ps.rs
@@ -6,7 +6,8 @@ use std::{
 
 use anyhow::Context;
 use fs_err::File;
-use sysinfo::{ProcessesToUpdate, System};
+use sysinfo::{Pid, ProcessRefreshKind, ProcessesToUpdate, System, UpdateKind, Users};
+use tabled::{settings::Style, Table, Tabled};
 
 use crate::cmds::pacman;
 
@@ -21,10 +22,56 @@ fn files_of_installed_pkgs() -> anyhow::Result<Vec<String>> {
     Ok(result)
 }
 
+#[derive(PartialEq, Eq, Hash, Tabled)]
+struct Process {
+    pid: Pid,
+    user_name: String,
+    command: String,
+}
+
+impl Process {
+    fn new(process: &sysinfo::Process) -> Self {
+        let pid = process.pid();
+
+        let command = process
+            .exe()
+            .map(|p| {
+                let file_name = p.file_name().unwrap_or_default().to_string_lossy();
+                file_name
+                    .strip_suffix("(deleted)")
+                    .map(ToString::to_string)
+                    .unwrap_or(file_name.to_string())
+            })
+            .unwrap_or_else(|| process.name().to_string_lossy().to_string())
+            .to_string();
+
+        let user_name = user_name_by_process(process).unwrap_or_else(|| String::from("Unknown"));
+
+        Self {
+            pid,
+            user_name,
+            command,
+        }
+    }
+}
+
+fn user_name_by_process(process: &sysinfo::Process) -> Option<String> {
+    let uid = process.user_id()?;
+    let users = Users::new_with_refreshed_list();
+    let user = users.get_user_by_id(uid)?;
+    Some(user.name().to_owned())
+}
+
 /// Returns (process_name, file_name)
-fn deleted_files_and_his_processes() -> anyhow::Result<Vec<(String, String)>> {
+fn deleted_files_and_his_processes() -> anyhow::Result<Vec<(Process, String)>> {
     let mut system = System::new();
-    system.refresh_processes(ProcessesToUpdate::All, true);
+    system.refresh_processes_specifics(
+        ProcessesToUpdate::All,
+        false,
+        ProcessRefreshKind::new()
+            .with_user(UpdateKind::OnlyIfNotSet)
+            .with_exe(UpdateKind::OnlyIfNotSet),
+    );
     let mut result = Vec::new();
     for (pid, process) in system.processes() {
         let path = Path::new("/proc").join(pid.to_string()).join("maps");
@@ -48,19 +95,7 @@ fn deleted_files_and_his_processes() -> anyhow::Result<Vec<(String, String)>> {
                 continue;
             }
 
-            let process = process
-                .exe()
-                .map(|p| {
-                    let file_name = p.file_name().unwrap_or_default().to_string_lossy();
-                    file_name
-                        .strip_suffix("(deleted)")
-                        .map(ToString::to_string)
-                        .unwrap_or(file_name.to_string())
-                })
-                .unwrap_or_else(|| process.name().to_string_lossy().to_string())
-                .to_string();
-
-            result.push((process, fname.to_owned()));
+            result.push((Process::new(process), fname.to_owned()));
         }
     }
     Ok(result)
@@ -71,11 +106,13 @@ pub fn ps() -> anyhow::Result<()> {
     let mut processes = HashSet::new();
     for (process, file) in deleted_files_and_his_processes()? {
         if pkgs_files.contains(&file) {
-            processes.insert(process.to_owned());
+            processes.insert(process);
         }
     }
-    for process in processes {
-        println!("{process}");
+    if processes.is_empty() {
+        return Ok(());
     }
+    let table = Table::new(&processes).with(Style::rounded()).to_string();
+    println!("{table}");
     Ok(())
 }

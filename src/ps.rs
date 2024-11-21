53cda78fb34f97e8d084815d4feb14986b9a57f7
Artyom Kalin <kalin.a.v20031@gmail.com>
2024-11-21 05:03:16 +0300

    refactor

--- src/ps.rs
+++ src/ps.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     io::{BufRead, BufReader},
     path::Path,
 };
@@ -72,37 +72,47 @@ fn configured_system() -> System {
     system
 }
 
-async fn deleted_files_and_his_processes() -> anyhow::Result<Vec<(Process, String)>> {
+fn process_has_deleted_files(pid: &Pid) -> anyhow::Result<HashSet<String>> {
+    let mut result = HashSet::new();
+    let path = Path::new("/proc").join(pid.to_string()).join("maps");
+    let Ok(file) = File::open(path) else {
+        return Ok(result);
+    };
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.split(' ').filter(|s| !s.is_empty()).collect();
+        if parts[parts.len() - 1] != "(deleted)" {
+            continue;
+        }
+        let fname = parts[parts.len() - 2];
+        if fname.starts_with("/dev")
+            || fname.starts_with("/run")
+            || fname.starts_with("/drm")
+            || fname.starts_with("/memfd")
+            || fname.starts_with("/SYSV")
+        {
+            continue;
+        }
+
+        result.insert(fname.to_owned());
+    }
+
+    Ok(result)
+}
+
+async fn deleted_files_and_his_processes() -> anyhow::Result<HashMap<Process, HashSet<String>>> {
     let (system, users) = join!(
         tokio::spawn(async { configured_system() }),
         tokio::spawn(async { Users::new_with_refreshed_list() })
     );
     let (system, users) = (system?, users?);
 
-    let mut result = Vec::new();
+    let mut result = HashMap::new();
     for (pid, process) in system.processes() {
-        let path = Path::new("/proc").join(pid.to_string()).join("maps");
-        let Ok(file) = File::open(path) else {
-            continue;
-        };
-        let reader = BufReader::new(file);
-        for line in reader.lines() {
-            let line = line?;
-            let parts: Vec<&str> = line.split(' ').filter(|s| !s.is_empty()).collect();
-            if parts[parts.len() - 1] != "(deleted)" {
-                continue;
-            }
-            let fname = parts[parts.len() - 2];
-            if fname.starts_with("/dev")
-                || fname.starts_with("/run")
-                || fname.starts_with("/drm")
-                || fname.starts_with("/memfd")
-                || fname.starts_with("/SYSV")
-            {
-                continue;
-            }
-
-            result.push((Process::new(process, &users), fname.to_owned()));
+        let files = process_has_deleted_files(pid)?;
+        if !files.is_empty() {
+            result.insert(Process::new(process, &users), files);
         }
     }
     Ok(result)
@@ -118,8 +128,8 @@ pub async fn ps() -> anyhow::Result<()> {
     let deleted_files_and_his_processes = deleted_files_and_his_processes??;
 
     let mut processes = HashSet::new();
-    for (process, file) in deleted_files_and_his_processes {
-        if pkgs_files.contains(&file) {
+    for (process, files) in deleted_files_and_his_processes {
+        if files.iter().any(|f| pkgs_files.contains(f)) {
             processes.insert(process);
         }
     }

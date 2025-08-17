26a66c1ebabc25ddcf66cee704aa18006ab61c66
Artyom Kalin <kalin.a.v20031@gmail.com>
2025-08-17 21:06:55 +0300

    build: better naming for function

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::Write, path::Path};
 
 use clap::{Command, CommandFactory};
-use clap_complete::{Generator, Shell, generate};
+use clap_complete::{Generator, Shell};
 
 include!("src/args.rs");
 
@@ -10,15 +10,15 @@ const SHELLS: [(Shell, &str); 2] = [(Shell::Bash, "bash"), (Shell::Zsh, "zsh")];
 fn main() {
     let mut args = Args::command();
     for (shell, shell_name) in SHELLS {
-        let content = g(&mut args, shell);
+        let content = generate(&mut args, shell);
         let path = Path::new("completions").join(shell_name);
         let mut file = File::create(path).expect("Failed to create file");
         file.write_all(&content).unwrap();
     }
 }
 
-fn g(cmd: &mut Command, generator: impl Generator) -> Vec<u8> {
+fn generate(cmd: &mut Command, generator: impl Generator) -> Vec<u8> {
     let mut buf = Vec::new();
-    generate(generator, cmd, "pacrs", &mut buf);
+    clap_complete::generate(generator, cmd, "pacrs", &mut buf);
     buf
 }
